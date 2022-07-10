b905f99272c3cae6140fb957a96b865694dc676e
Max Jenkins <mjen442@aucklanduni.ac.nz>
2022-07-10 16:17:41 +1200

    converted to ticks and works but not connected to GUI selection yet.

--- src/main.py
+++ src/main.py
@@ -5,36 +5,8 @@ import linear_controller
 import logging
 from queue import Queue
 import global_variables_flags
-
-
-if __name__ == "__main__":
-    capacity, charge, discharge_rate = 20, 10, 2 # capacity in KWH, discharge rate in KW,
-    max_value, angular_frequency, prediction_length = 5, 1/86400, 10
-    update_rate_buffer = 0.5
-    lower_limit, upper_limit = capacity*0.3, capacity*0.7
-    time_multiplier = 3600
-    count = 0
-    control_scheme = 'linear'
-
-    default_battery = Battery(capacity, charge, discharge_rate)
-    while True:
-        time.sleep(1)
-        prediction_interval = generateValues.generate_prediction_interval(max_value, angular_frequency,
-                                                                          prediction_length, count*time_multiplier)
-        immediate_prediction = generateValues.generate_current_value(max_value, angular_frequency, count*time_multiplier)
-        new_discharge_rate = linear_controller.linear_control(default_battery.get_charge(),
-                                                                    default_battery.get_max_discharge_rate(),
-                                                                    immediate_prediction, prediction_interval,
-                                                                    lower_limit, upper_limit, update_rate_buffer,
-                                                                    default_battery.get_discharge_rate())
-
-        default_battery.set_discharge_rate(new_discharge_rate)
-        logging.info("Tick: {} - {} hours elapsed - using {} control".format(count, count*time_multiplier/3600, 'linear'))
-        logging.info("Battery charge: {}KWH, input power: {}KW, discharge rate: {}KW".format(round(default_battery.get_charge(), 4),
-                                                                                      immediate_prediction,
-                                                                                      round(new_discharge_rate, 4)))
-        default_battery.update(immediate_prediction)
-        count = count + 1
+import pid_body
+import pid_controller
 
 display_queue = Queue()
 
@@ -46,20 +18,30 @@ def run_through_gui():
     lower_limit, upper_limit = capacity * 0.3, capacity * 0.7
     time_multiplier = 3600
     count = 0
-    control_scheme = 'linear'
+    control_scheme = 'pid'
 
     default_battery = Battery(capacity, charge, discharge_rate)
+    PID = pid_body.PID(0.5, 0, 0.5, setpoint=0)
+    PID.output_limits = (-5, 0)  # just don't ask
+
+    start_tick = count
+    last_tick = start_tick
+
     while True:
         time.sleep(1)
         prediction_interval = generateValues.generate_prediction_interval(max_value, angular_frequency,
                                                                           prediction_length, count * time_multiplier)
         immediate_prediction = generateValues.generate_current_value(max_value, angular_frequency,
                                                                      count * time_multiplier)
-        new_discharge_rate = linear_controller.linear_control(default_battery.get_charge(),
+        if control_scheme == 'linear':
+            new_discharge_rate = linear_controller.linear_control(default_battery.get_charge(),
                                                               default_battery.get_max_discharge_rate(),
                                                               immediate_prediction, prediction_interval,
                                                               lower_limit, upper_limit, update_rate_buffer,
                                                               default_battery.get_discharge_rate())
+        elif control_scheme == 'pid':
+
+            count, last_tick, new_discharge_rate = pid_controller.pid_control(count, last_tick, default_battery.get_charge(), PID)
 
         default_battery.set_discharge_rate(new_discharge_rate)
         display_queue.put("Tick: {} - {} hours elapsed - using {} control".format(count, count * time_multiplier / 3600, 'linear'))
